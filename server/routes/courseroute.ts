a6f50fdda28966473be02f7d8f223a9f2b02ab82
m-iftikhar <iftikharrj2001@gmail.com>
2025-02-04 12:59:11 +0500

    add get one & all controllers

--- server/routes/courseroute.ts
+++ server/routes/courseroute.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import { editCourse, uploadCourse } from '../controllers/coursecontroller';
+import { editCourse, getAllCourses, getSingleCourse, uploadCourse } from '../controllers/coursecontroller';
 import { authorizeRoles, isAutheticated } from '../middleware/auth';
 
 const courseRouter= express.Router();
 
 courseRouter.post("/uploadcourse",isAutheticated,authorizeRoles("admin"), uploadCourse);
 courseRouter.put("/editcourse/:id",isAutheticated,authorizeRoles("admin"), editCourse);
+courseRouter.get("/getcourse/:id", getSingleCourse);
+courseRouter.get("/getallcourse", getAllCourses);
 
 
 
