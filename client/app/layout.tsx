ded525d0f0b70b2a16cf7d49c6f73d4a143e2f9d
m-iftikhar <iftikharrj2001@gmail.com>
2025-04-07 18:15:41 +0500

    provider done

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,10 +1,10 @@
 "use client";
 import "./globals.css";
-
+import { Toaster } from "react-hot-toast";
 import { Poppins } from "next/font/google";
 import { Josefin_Sans } from "next/font/google";
 import { ThemeProvider } from "./utils/theme-provider";
-
+import { Providers } from "./Provider";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -30,10 +30,12 @@ export default function RootLayout({
       <body
         className={`${poppins.variable} ${josefin.variable} !bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}
       >
-     
+       <Providers>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
+          <Toaster position="top-center" reverseOrder={false} />
         </ThemeProvider>
+        </Providers>
       </body>
     </html>
   );
